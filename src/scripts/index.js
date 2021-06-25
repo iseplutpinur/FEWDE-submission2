9272ca10f75698e2a2d2d0be47d289f6c3a88a20
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-06-26 03:02:32 +0700

    update apps

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,10 @@
 import 'regenerator-runtime'
 import '../styles/style.css'
 import '../styles/responsive.css'
+import App from './views/app'
 
-console.log('Hello Coders!')
+const app = new App({
+  button: document.querySelector('#menu'),
+  drawer: document.querySelector('#nav_drawer'),
+  content: document.querySelector('#mainContent')
+})
