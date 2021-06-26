0afe1fa6a82f673e86cbf73d75cfce3636b3daf4
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-06-26 16:10:36 +0700

    update cache api

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,8 +2,9 @@ const CONFIG = {
   KEY: '12345',
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
-  BASE_IMAGE_URL_SMALL: 'https://restaurant-api.dicoding.dev/images/small/'
-  // DEFAULT_LANGUAGE: 'en-us',
+  BASE_IMAGE_URL_SMALL: 'https://restaurant-api.dicoding.dev/images/small/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'cari-makan-appsv1'
   // DATABASE_NAME: 'cari-makan-apps',
   // DATABASE_VERSION: 1,
   // OBJECT_STORE_NAME: 'favorite'
